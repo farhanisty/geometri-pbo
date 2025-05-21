ed01b8d2b36a063b5cd5f82aedf3d6e93cff9f60
farhanisty <farhannivtasija@gmail.com>
2025-05-21 22:53:58 +0700

    create Bola

---
+++ src/geometri/benda/geometri/lingkaran/Bola.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package geometri.benda.geometri.lingkaran;
+
+/**
+ *
+ * @author farhannivta
+ */
+public class Bola extends Lingkaran {
+    
+    public Bola(double jariJari) {
+        super(jariJari);
+    }
+    
+    public double hitungVolume() {
+        return Math.pow(this.jariJari, 3) * Lingkaran.PI * 4 / 3;
+    }
+    
+    @Override
+    public double hitungLuas() {
+        return Math.pow(this.jariJari, 2) * Lingkaran.PI * 4;
+    }
+    
+    @Override
+    public String getNama() {
+        return "Bola";
+    }
+}
